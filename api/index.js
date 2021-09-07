df29ba4373728f47607061448179f1cecd915d20
Salih Gumus <sudanmerinosu@gmail.com>
2021-09-07 10:32:13 +0300

    small fix

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-var cors = require('cors');
+const cors = require('cors');
 
 require('dotenv').config();
 
